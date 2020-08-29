dc7c1066e1ee1861799f7d3f5c9bf3d75fd578b0
Magnus Jensen <magnus.jensen@isobar.com>
2020-08-29 10:02:13 +0200

    initial

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+            for(int i = 0; i < 10; i++){
+                System.out.println("Jeg ved, at jeg har været dygtig " + i + " gange");
+            }
+        }
+}
